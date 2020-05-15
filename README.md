c0b02f41a267c785176fe935f1bf0a548640128f
Anubhav Shukla <ashukla@fispan.com>
2020-05-14 21:02:25 -0700

    Initial Repository setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# RestTemplate Request Logger
---
+++ src/main/java/com/actigence/rrl/App.java
@@ -0,0 +1,12 @@
+package com.actigence.rrl;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
